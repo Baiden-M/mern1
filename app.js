7a9097997dd5709750545a50764079c9451ca1e7
Baiden-M <99929085+Baiden-M@users.noreply.github.com>
2022-10-29 22:18:05 -0600

    add link

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = 3061
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+  res.send('Baiden McElroy s73j573')
+  res.send('https://github.com/Baiden-M/mern1.git')
 })
 
 app.listen(port, () => {
